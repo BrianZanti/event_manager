1be1a1e8cae7c21c3c5bbace7ab8ac0582484d88
Brian Zanti <brianzanti@gmail.com>
2018-03-29 15:10:29 -0600

    tests now use class method

--- test/file_io_test.rb
+++ test/file_io_test.rb
@@ -5,10 +5,10 @@ require 'pry'
 
 class FileIOTest < Minitest::Test
   def test_it_reads_file
-    em = FileIO.new("./data/full_event_attendees.csv")
-    assert_equal 5175, em.attendees.length
-    assert_equal "Allison Nguyen", em.attendees.first
-    assert_equal "X Lee", em.attendees[2246]
-    assert_equal "Greg SAUNDERS", em.attendees.last
+    attendees = FileIO.load("./data/full_event_attendees.csv")
+    assert_equal 5175, attendees.length
+    assert_equal "Allison Nguyen", attendees.first
+    assert_equal "X Lee", attendees[2246]
+    assert_equal "Greg SAUNDERS", attendees.last
   end
 end
